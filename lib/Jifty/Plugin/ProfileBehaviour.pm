10861f0a260a082bdabe8f27e70276d27da4e2ab
Thomas Sibley <trs@bestpractical.com>
2010-01-15 14:13:12 -0500

    Fix spelling and bump version

--- lib/Jifty/Plugin/ProfileBehaviour.pm
+++ lib/Jifty/Plugin/ProfileBehaviour.pm
@@ -4,15 +4,15 @@ use warnings;
 package Jifty::Plugin::ProfileBehaviour;
 use base qw/Jifty::Plugin/;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 NAME
 
-Jifty::Plugin::ProfileBehaviour - Overrides behavior.js to add profiling information
+Jifty::Plugin::ProfileBehaviour - Overrides behaviour.js to add profiling information
 
 =head1 DESCRIPTION
 
-This plugin overrides the stock behavior.js library to add timing and
+This plugin overrides the stock behaviour.js library to add timing and
 profiling information.  Add it if your web pages are slow to style,
 and you want to track down which rules are causing the slowness.
 
@@ -25,7 +25,7 @@ Adds the CSS file needed for on-screen profiling.
 =cut
 
 sub init {
-    Jifty->web->add_css('behavior-profile.css');
+    Jifty->web->add_css('behaviour-profile.css');
 }
 
 1;
